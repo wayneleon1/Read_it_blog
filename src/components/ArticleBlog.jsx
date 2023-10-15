cb879e1e8e900b6735f6fdd14c91ae7011bf0256
Rurangwa Leo <88591087+wayneleon1@users.noreply.github.com>
2023-10-15 17:59:14 +0200

    Displaying Dynomic data in Artile page

--- src/components/ArticleBlog.jsx
+++ src/components/ArticleBlog.jsx
@@ -1,39 +1,41 @@
 import BlogImg from "../Images/Blog-Image.jpg";
 import "../components/style/ArticleBlog.css";
-export default function ArticleBlog() {
+const ArticleBlog = ({ title, image, desc, day, month, year }) => {
   return (
     <>
       <div className="post">
         <div className="post-img">
-          <img src={BlogImg} alt="" />
+          <img src={image} alt="" />
         </div>
         <div className="topper">
           <div className="topper-content">
             <div className="one">
-              <span className="day">18</span>
+              <span className="day">{day}</span>
             </div>
             <div className="two">
-              <p class="yr">2019</p>
-              <p class="mos">October</p>
+              <p class="yr">{year}</p>
+              <p class="mos">{month}</p>
             </div>
           </div>
         </div>
         <div className="post-body">
           <div className="post-title">
-            <h1>All you want to know about industrial laws</h1>
+            <h1>{title}</h1>
           </div>
           <div className="post-desc">
-            <p>
-              A small river named Duden flows by their place and supplies it
-              with the necessary regelialia.
-            </p>
+            <p>{desc}</p>
           </div>
           <div className="post-footer">
-            <iconify-icon icon="eva:arrow-forward-fill"></iconify-icon>
-            <p>Read more</p>
+            <div>
+              <iconify-icon icon="eva:arrow-forward-fill"></iconify-icon>
+            </div>
+            <div>
+              <p>Read more</p>
+            </div>
           </div>
         </div>
       </div>
     </>
   );
-}
+};
+export default ArticleBlog;
