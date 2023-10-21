22daf2af498974c3007427cc01b55fcdd2b31ff5
Rurangwa Leo <88591087+wayneleon1@users.noreply.github.com>
2023-10-21 22:36:22 +0200

    Login to Dashboard using real api

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,27 +1,65 @@
 import "../components/style/login.css";
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+
 export default function Login() {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  };
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+  };
+  const handleApi = () => {
+    axios
+      .post("https://node-app-plsi.onrender.com/api/klab/user/login", {
+        email: email,
+        password: password,
+      })
+      .then((result) => {
+        alert("Login Successfully");
+        navigate("/dashboard");
+      })
+      .catch((error) => {
+        alert("Incorrect email and password");
+        setEmail("");
+        setPassword("");
+        console.log(error);
+      });
+  };
+
   return (
     <>
       <div className="Login-section container">
-        <form>
-          <div className="login-container">
-            <h4>
-              Readit!<span className="span"> Welcome</span> Back
-            </h4>
-            <input type="text" placeholder="Username" />
-            <input type="password" placeholder="Password" />
+        <div className="login-container">
+          <h4>
+            Readit!<span className="span"> Welcome</span> Back
+          </h4>
 
-            <button>Login</button>
+          <input
+            type="email"
+            value={email}
+            onChange={handleEmail}
+            placeholder="Email"
+          />
+          <input
+            type="password"
+            value={password}
+            onChange={handlePassword}
+            placeholder="Password"
+          />
+          <button onClick={handleApi}>Login</button>
 
-            <p>
-              Don't have any account?{" "}
-              <Link to="/signup">
-                <span className="link">Register Here</span>
-              </Link>
-            </p>
-          </div>
-        </form>
+          <p>
+            Don't have any account?{" "}
+            <Link to="/signup">
+              <span className="link">Register Here</span>
+            </Link>
+          </p>
+        </div>
       </div>
     </>
   );
