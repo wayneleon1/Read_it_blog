4c98d234e1ea369c8c961a73b309b8570a9e4616
Reconfort Daniel <reconfortdanny@gmail.com>
2023-10-23 23:46:05 +0200

    working on dashboard cards

--- src/dashboard/pages/Dashboard.jsx
+++ src/dashboard/pages/Dashboard.jsx
@@ -1,11 +1,55 @@
-import { useNavigate } from "react-router-dom";
 import "../components/style/dashboard.css";
-import { useEffect } from "react";
+
 export default function Dashboard() {
   return (
     <>
       <div className="dashboard-section container-section">
-        <h2>Welcome to Dashboard Page</h2>
+        <div className="cards-container">
+          <div className="dash-cards gold">
+            <div className="col-1">
+              <h3>23+</h3>
+              <h4>Total Posts</h4>
+            </div>
+            <div className="col-2">
+              <div className="dash-icon">
+                <iconify-icon icon="mdi:post-it-note-check"></iconify-icon>
+              </div>
+            </div>
+          </div>
+          <div className="dash-cards dodge-blue">
+            <div className="col-1">
+              <h3>23+</h3>
+              <h4>Total Posts</h4>
+            </div>
+            <div className="col-2">
+              <div className="dash-icon">
+                <iconify-icon icon="mdi:post-it-note-check"></iconify-icon>
+              </div>
+            </div>
+          </div>
+          <div className="dash-cards gray">
+            <div className="col-1">
+              <h3>23+</h3>
+              <h4>Total Posts</h4>
+            </div>
+            <div className="col-2">
+              <div className="dash-icon">
+                <iconify-icon icon="mdi:post-it-note-check"></iconify-icon>
+              </div>
+            </div>
+          </div>
+          <div className="dash-cards tomato">
+            <div className="col-1">
+              <h3>23+</h3>
+              <h4>Total Posts</h4>
+            </div>
+            <div className="col-2">
+              <div className="dash-icon">
+                <iconify-icon icon="mdi:post-it-note-check"></iconify-icon>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
